39001ed87f7751e19c840b94808b0794d77a5fc8
Michal Boguszewski <michal1842001@gmail.com>
2022-11-17 23:04:36 +0100

    Create User

---
+++ src/Domain/Users/User.cs
@@ -0,0 +1,13 @@
+namespace Domain.Users;
+
+public class User
+{
+    public Guid Id { get; private init; }
+    public PersonalData PersonalData { get; private set; }
+
+    public User(PersonalData personalData)
+    {
+        Id = Guid.NewGuid();
+        PersonalData = personalData;
+    }
+}
