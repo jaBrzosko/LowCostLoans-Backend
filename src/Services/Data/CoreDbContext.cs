686fba8de185c60f20cb965fa17b4e7beaa5c110
Michal Boguszewski <michal1842001@gmail.com>
2022-11-17 23:22:02 +0100

    Add User and Inquire to CoreDbContext

--- src/Services/Data/CoreDbContext.cs
+++ src/Services/Data/CoreDbContext.cs
@@ -1,4 +1,6 @@
 using Domain.Examples;
+using Domain.Inquires;
+using Domain.Users;
 using Microsoft.EntityFrameworkCore;
 
 namespace Services.Data;
@@ -6,7 +8,9 @@ namespace Services.Data;
 public class CoreDbContext : DbContext
 {
     public DbSet<Example> Examples { get; set; }
-    
+    public DbSet<User> Users { get; set; }
+    public DbSet<Inquire> Inquires { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseInMemoryDatabase(databaseName: "CoreDb");
@@ -15,11 +19,41 @@ public class CoreDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         ConfigureExamples(modelBuilder);
+        ConfigureUsers(modelBuilder);
+        ConfigureInquires(modelBuilder);
     }
 
     private static void ConfigureExamples(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Example>().HasKey(e => e.Id);
         modelBuilder.Entity<Example>().Property(e => e.Name).HasMaxLength(StringLengths.ShortString);
+    }
+    
+    private static void ConfigureUsers(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(cfg =>
+        {
+            cfg.HasKey(e => e.Id);
+            cfg.OwnsOne(e => e.PersonalData, inner =>
+            {
+                inner.Property(e => e.FirstName).HasMaxLength(StringLengths.ShortString);
+                inner.Property(e => e.LastName).HasMaxLength(StringLengths.ShortString);
+                inner.Property(e => e.GovernmentId).HasMaxLength(StringLengths.MediumString);
+            });
+        });
+    }
+    
+    private static void ConfigureInquires(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Inquire>(cfg =>
+        {
+            cfg.HasKey(e => e.Id);
+            cfg.OwnsOne(e => e.PersonalData, inner =>
+            {
+                inner.Property(e => e.FirstName).HasMaxLength(StringLengths.ShortString);
+                inner.Property(e => e.LastName).HasMaxLength(StringLengths.ShortString);
+                inner.Property(e => e.GovernmentId).HasMaxLength(StringLengths.MediumString);
+            });
+        });
     }
 }
--- src/Services/Data/StringLengths.cs
+++ src/Services/Data/StringLengths.cs
@@ -2,5 +2,6 @@ namespace Services.Data;
 
 public static class StringLengths
 {
-    public const int ShortString = 50;
+    public const int ShortString = 250;
+    public const int MediumString = 500;
 }
