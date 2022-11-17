052a3a248da570a2af7d825a005ea3a4ae5babe0
Michal Boguszewski <michal1842001@gmail.com>
2022-11-17 23:10:41 +0100

    Create Inquire

---
+++ src/Domain/Inquires/Inquire.cs
@@ -0,0 +1,33 @@
+using Domain.Users;
+
+namespace Domain.Inquires;
+
+public class Inquire
+{
+    public Guid Id { get; private init; }
+    public Guid? UserId { get; private init; }
+    public PersonalData? PersonalData { get; private init; }
+    public int MoneyInSmallestUnit { get; private init; }
+    public int NumberOfInstallments { get; private init; }
+    public DateTime CreationTime { get; private init; }
+
+    public Inquire(Guid? userId, PersonalData? personalData, int moneyInSmallestUnit, int numberOfInstallments)
+    {
+        Validate(userId, personalData);
+        
+        Id = Guid.NewGuid();
+        UserId = userId;
+        PersonalData = personalData;
+        MoneyInSmallestUnit = moneyInSmallestUnit;
+        NumberOfInstallments = numberOfInstallments;
+        CreationTime = DateTime.UtcNow;
+    }
+
+    private static void Validate(Guid? userId, PersonalData? personalData)
+    {
+        if (!(userId is null ^ personalData is null))
+        {
+            throw new ArgumentException("Exactly one of userId and personalData has to be null");
+        }
+    }
+}
