b510587c21bc6d0d72743157cfad68bd58ce6e36
Michal Boguszewski <michal1842001@gmail.com>
2022-11-19 19:00:24 +0100

    Test creating Offer

---
+++ tests/Domain.UnitTests/Offers/OfferCreationTests.cs
@@ -0,0 +1,54 @@
+using Domain.Offers;
+using FluentAssertions;
+using Xunit;
+
+namespace Domain.UnitTests.Offers;
+
+public class OfferCreationTests
+{
+    [Fact]
+    public void Create_with_normal_values()
+    {
+        var expectedInquireId = Guid.NewGuid();
+        int expectedInterestRate = 100;
+        int expectedMoney = 10_000;
+        int expectedNumberOfInstallments = 12;
+        
+        var actualOffer = new Offer(expectedInquireId, expectedInterestRate, expectedMoney, expectedNumberOfInstallments);
+
+        actualOffer.InquireId.Should().Be(expectedInquireId);
+        actualOffer.InterestRate.Should().Be(expectedInterestRate);
+        actualOffer.MoneyInSmallestUnit.Should().Be(expectedMoney);
+        actualOffer.NumberOfInstallments.Should().Be(expectedNumberOfInstallments);
+    }
+
+    [Fact]
+    public void Money_is_zero()
+    {
+        TestThrowingArgumentException(Guid.NewGuid(), 100, 0, 12);
+    }
+    
+    [Fact]
+    public void Money_is_negative()
+    {
+        TestThrowingArgumentException(Guid.NewGuid(), 100, -12, 12);
+    }
+    
+    [Fact]
+    public void NumberOfInstallments_is_zero()
+    {
+        TestThrowingArgumentException(Guid.NewGuid(), 100, 1000, 0);
+    }
+    
+    [Fact]
+    public void NumberOfInstallments_is_negative()
+    {
+        TestThrowingArgumentException(Guid.NewGuid(), 100, 1000, -1);
+    }
+    
+    private void TestThrowingArgumentException(Guid inquireId, int interestRate, int money, int numberOfInstallments)
+    {
+        var action = () => { new Offer(inquireId, interestRate, money, numberOfInstallments); };
+        action.Should().Throw<ArgumentException>();
+    }
+}
