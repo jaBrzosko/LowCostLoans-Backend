a7c39cefe411a43496bc519697516948fc76cd98
Birchman <jakubbrzoskowski@gmail.com>
2022-11-19 11:41:03 +0100

    Build and run backend docker image

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Run docker compose for this backend
+docker-compose -f src/docker-compose.yaml up -d --build
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Stop this backend
+docker-compose -f src/docker-compose.yaml down
