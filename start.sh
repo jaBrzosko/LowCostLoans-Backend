ca4301edd5f23e0713374027f631199b3e129a89
Birchman <jakubbrzoskowski@gmail.com>
2022-11-28 04:44:35 +0100

    Add API run at the same time

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+cd ..
+
+if [ ! -d "API" ] ; then
+  git clone git@ssh.dev.azure.com:v3/javadotnetproject/Loan/API API
+fi
+
+cd Backend
+
 # Run docker compose for this backend
 docker-compose -f src/docker-compose.yaml up -d --build
+
